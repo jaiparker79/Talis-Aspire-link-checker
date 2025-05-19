948e9c8c3f08e42a7a261792c5c14ff8fbe3c251
jaiparker79 <jaiparker79@gmail.com>
2025-05-19 10:50:35 +1000

    Update Talis_link_checker.ps1
    Fixed return for EBSCOhost to make URL

--- Talis_link_checker.ps1
+++ Talis_link_checker.ps1
@@ -7,7 +7,7 @@ function Test-Url {
 
     # Check for specific URL patterns first. The ones listed below are QUT-specific, please edit these for your own libraries needs.
     if ($url -match "^https://web\.[a-z0-9]+\.ebscohost\.com") {
-        return "EBSCOhost, see https://www.library.qut.edu.au/search/status/linking/#other"
+        return "https://www.library.qut.edu.au/search/status/linking/#other"
     } elseif ($url -match "^https://www\.clickview\.net/videos/") {
         return "https://www.library.qut.edu.au/search/status/linking/#other"
     } elseif ($url -match "^https://launch\.clickview\.net") {
