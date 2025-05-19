59a02f9f361f224f1a61462409e4c517dc1a199a
jaiparker79 <jaiparker79@gmail.com>
2025-05-19 22:01:11 +1000

    Update Talis_link_checker.ps1
    Increased timeoutsec to 90 to weed out a few false positives

--- Talis_link_checker.ps1
+++ Talis_link_checker.ps1
@@ -48,7 +48,7 @@ function Test-Url {
 
     while ($retryCount -lt $maxRetries -and $errorCode -eq $null) {
         try {
-            $response = Invoke-WebRequest -Uri $url -Method Head -TimeoutSec 30 -Headers @{"User-Agent"="Mozilla/5.0"} -MaximumRedirection 5 -ErrorAction Stop
+            $response = Invoke-WebRequest -Uri $url -Method Head -TimeoutSec 90 -Headers @{"User-Agent"="Mozilla/5.0"} -MaximumRedirection 5 -ErrorAction Stop
 
             # Handle redirections
             if ($response.StatusCode -ge 300 -and $response.StatusCode -lt 400) {
