ddff910cdef3f2e77e09bbb7174befcdd4e5660c
jaiparker79 <jaiparker79@gmail.com>
2025-08-20 14:52:49 +1000

    Update Talis_link_checker.ps1
    Added blank line after name of file being checked

--- Talis_link_checker.ps1
+++ Talis_link_checker.ps1
@@ -164,6 +164,7 @@ $outputFilename = "broken-links-$($inputFilename.BaseName).csv"
 try {
     if ($inputFilename) {
         Write-Host "`nChecking $($inputFilename.Name)" -ForegroundColor Magenta
+        Write-Host "" #Blank line
         $csv = Import-Csv -Path $inputFilename.FullName
         $output = @()
         $lineCount = 0
@@ -214,5 +215,6 @@ try {
 Read-Host -Prompt "Press Enter to exit"
 
 # End QUT Readings link checking script
+
 
 
